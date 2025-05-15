fb6b66cf09d929190f0189872dcae304c453e9c4
JerryCode777 <jerry_07_j@hotmail.com>
2025-05-14 23:49:04 -0500

    Initial Commit

---
+++ ReversorApp/ReversorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversorAppApp.swift
+//  ReversorApp
+//
+//  Created by Jerry Anderson Huaynacho Mango on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ReversorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReversorAppTests/ReversorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReversorAppTests.swift
+//  ReversorAppTests
+//
+//  Created by Jerry Anderson Huaynacho Mango on 14/05/25.
+//
+
+import Testing
+@testable import ReversorApp
+
+struct ReversorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
